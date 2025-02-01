80699f9f9329cecc9f2c800eaef4b51d10857224
Karma7869 <vlad78697869@gmail.com>
2025-02-01 15:46:38 +0500

    bug fix

--- app/src/test/java/MapSchemaTest.java
+++ app/src/test/java/MapSchemaTest.java
@@ -1,7 +1,6 @@
-package hexlet.code;
-
+import hexlet.code.Validator;
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
-import hexlet.code.schemas.StringSchema;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -17,7 +16,7 @@ class MapSchemaTest {
         Validator v = new Validator();
         MapSchema schema = v.map();
 
-        Map<String, StringSchema> schemas = new HashMap<>();
+        Map<String, BaseSchema<?>> schemas = new HashMap<>();
         schemas.put("firstName", v.string().required());
         schemas.put("lastName", v.string().required().minLength(2));
 
